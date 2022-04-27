393da640bd03cb25112bfd1f42042b1f7b6851f2
Darrenfisherlol <darren.nov@gmail.com>
2022-04-27 11:36:42 -0500

    almost done

--- L27ASPAddDeleteModify/Controllers/HomeController.cs
+++ L27ASPAddDeleteModify/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using L27ASPAddDeleteModify.Models;
+﻿using L27ASPAddDeleteModify.Data;
+using L27ASPAddDeleteModify.Models;
+
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,10 +15,66 @@ namespace L27ASPAddDeleteModify.Controllers
             _logger = logger;
         }
 
+
+
+
+
         public IActionResult Index()
+        {
+
+            using StuDBContext _db = new StuDBContext();
+
+            Student stu = new Student { id = "101", name = "Pickle", grade = 90 };
+            _db.Students.Add(stu);
+
+            stu = new Student { id = "102", name = "Rick", grade = 95 };
+            _db.Students.Add(stu);
+
+            stu = new Student { id = "102", name = "Morty", grade = 87 };
+            _db.Students.Add(stu);
+
+            _db.SaveChanges();
+
+
+            return View();
+        }
+
+        public IActionResult Add()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Add(Student stu)
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            
+            using StuDBContext db = new StuDBContext();
+
+            Student[] stuArray = db.Students.ToArray<Student>();
+
+
+            return View(stuArray);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+
+
+
+
 
         public IActionResult Privacy()
         {
